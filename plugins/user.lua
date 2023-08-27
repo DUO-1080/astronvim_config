6e4892a0caa6281cc314a7af72beaf6cc8311099
zenon <duo.1080@gmail.com>
2023-08-27 23:32:00 +0800

    use astrocommunity

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,26 +16,6 @@ return {
     end,
   },
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup {
-        filesystem = {
-          window = {
-            mappings = {
-              ["<leader>p"] = "image_wezterm", -- " or another map
-            },
-          },
-          commands = {
-            image_wezterm = function(state)
-              local node = state.tree:get_node()
-              if node.type == "file" then require("image_preview").PreviewImage(node.path) end
-            end,
-          },
-        },
-      }
-    end,
-  },
-  {
     "neanias/everforest-nvim",
     version = false,
     lazy = false,
@@ -48,30 +28,6 @@ return {
       }
     end,
   },
-
-  {
-    "edluffy/hologram.nvim",
-    event = "User AstroFile",
-    config = function()
-      require("hologram").setup {
-        auto_display = true, -- WIP automatic markdown image display, may be prone to breaking
-      }
-    end,
-  },
-  {
-    "adelarsq/image_preview.nvim",
-    event = "VeryLazy",
-    config = function() require("image_preview").setup() end,
-  },
-  -- {
-  --   "folke/trouble.nvim",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   opts = {
-  --     -- your configuration comes here
-  --     -- or leave it empty to use the default settings
-  --     -- refer to the configuration section below
-  --   },
-  -- },
   {
     "p00f/clangd_extensions.nvim",
     event = "BufRead",
@@ -84,70 +40,5 @@ return {
   {
     "preservim/tagbar",
     event = "BufRead",
-  },
-  {
-    "rebelot/heirline.nvim",
-    opts = function(_, opts)
-      local status = require "astronvim.utils.status"
-      opts.statusline = { -- statusline
-        hl = { fg = "fg", bg = "bg" },
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-        status.component.git_branch(),
-        status.component.file_info { filetype = {}, filename = false, file_modified = false },
-        status.component.git_diff(),
-        status.component.diagnostics(),
-        status.component.fill(),
-        status.component.cmd_info(),
-        status.component.fill(),
-        status.component.lsp(),
-        status.component.treesitter(),
-        status.component.nav(),
-        -- remove the 2nd mode indicator on the right
-      }
-
-      opts.winbar = { -- create custom winbar
-        -- store the current buffer number
-        init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-        fallthrough = false, -- pick the correct winbar based on condition
-        -- inactive winbar
-        {
-          condition = function() return not status.condition.is_active() end,
-          -- show the path to the file relative to the working directory
-          status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
-          -- add the file name and icon
-          status.component.file_info {
-            file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
-            file_modified = false,
-            file_read_only = false,
-            hl = status.hl.get_attributes("winbarnc", true),
-            surround = false,
-            update = "BufEnter",
-          },
-        },
-        -- active winbar
-        {
-          -- show the path to the file relative to the working directory
-          status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
-          -- add the file name and icon
-          status.component.file_info { -- add file_info to breadcrumbs
-            file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
-            file_modified = false,
-            file_read_only = false,
-            hl = status.hl.get_attributes("winbar", true),
-            surround = false,
-            update = "BufEnter",
-          },
-          -- show the breadcrumbs
-          status.component.breadcrumbs {
-            icon = { hl = true },
-            hl = status.hl.get_attributes("winbar", true),
-            prefix = true,
-            padding = { left = 0 },
-          },
-        },
-      }
-      -- return the final configuration table
-      return opts
-    end,
   },
 }
