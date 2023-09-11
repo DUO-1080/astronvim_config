010e42a0b0fd29116d14c0cb476426e32451235f
zenon <duo.1080@gmail.com>
2023-09-11 22:15:41 +0800

    set scrolloff value

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = true, -- sets vim.opt.spell
     signcolumn = "yes", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    scrolloff = 10,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
--- plugins/user.lua
+++ plugins/user.lua
@@ -14,20 +14,21 @@ return {
         show_current_context = false,
       }
     end,
+    config = function() require("indent_blankline").setup {} end,
   },
-  {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- Optional; default configuration will be used if setup isn't called.
-    config = function()
-      require("everforest").setup {
-        -- Your config here
-        background = "hard",
-      }
-    end,
-  },
+  -- {
+  --   "neanias/everforest-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --     require("everforest").setup {
+  --       -- Your config here
+  --       background = "hard",
+  --     }
+  --   end,
+  -- },
   {
     "p00f/clangd_extensions.nvim",
     event = "BufRead",
